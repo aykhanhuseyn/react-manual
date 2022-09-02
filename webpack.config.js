dac78287bc038cb9ea2248e41e752c48acbab92a
Aykhan Huseyn <immense_ideas@icloud.com>
2022-09-02 20:59:33 +0400

    feat: add webpack and babel react

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV == 'production';
+
+const stylesHandler = 'style-loader';
+
+const config = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: 'main.js',
+	},
+	devServer: {
+		static: {
+			directory: path.join(__dirname, './build'),
+		},
+		compress: true,
+		open: true,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './index.html',
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/i,
+				loader: 'babel-loader',
+			},
+			{
+				test: /\.css$/i,
+				use: [stylesHandler, 'css-loader'],
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [stylesHandler, 'css-loader', 'sass-loader'],
+			},
+			{
+				test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+				type: 'asset',
+			},
+
+			// Add your rules for custom modules here
+			// Learn more about loaders from https://webpack.js.org/loaders/
+		],
+	},
+};
+
+module.exports = () => {
+	if (isProduction) {
+		config.mode = 'production';
+	} else {
+		config.mode = 'development';
+	}
+	return config;
+};
